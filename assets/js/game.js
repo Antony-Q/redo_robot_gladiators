265df48ad2a50db83c6665df4d76ec2281e1bb91
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-09 13:24:50 -0500

    added console logs sample

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,11 @@
 var playerName = window.prompt("What is your robot's name?");
 // debug
 console.log(playerName);
+console.log("This logs a string, good for leaving yourself a mesage.");
+// this will do math and log 20
+console.log(10 + 10);
+// c log players name
+console.log("Our robot's name is " + playerName);
 
 // this creates a function names "fight"
 function fight() {
